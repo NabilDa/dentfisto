ef6fcb4d90f57f87fd5916230d575fa14e37e3b1
mouaaaaaadd <mouaaaaaadd@users.noreply.github.com>
2026-04-06 16:06:44 +0100

    Email sending fixed . PROJECT COMPLETED

--- src/main/java/com/dentfisto/util/EmailUtil.java
+++ src/main/java/com/dentfisto/util/EmailUtil.java
@@ -10,14 +10,18 @@ import java.util.Properties;
 public class EmailUtil {
 
     private static final String FROM_EMAIL = "tt01taha01@gmail.com";
-    private static final String FROM_PASSWORD = "dentfistofst";
+    // IMPORTANT: Use a Gmail "App Password", NOT your regular password.
+    // Generate one at: https://myaccount.google.com/apppasswords (2FA must be
+    // enabled)
+    // A 16-char code like "abcd efgh ijkl mnop" (without spaces)
+    private static final String FROM_PASSWORD = "bkbn zqgg vjuo ujut";
 
     /**
      * Send an HTML email to the given recipient.
      *
-     * @param toEmail   recipient email address
-     * @param subject   email subject
-     * @param htmlBody  HTML content of the email body
+     * @param toEmail  recipient email address
+     * @param subject  email subject
+     * @param htmlBody HTML content of the email body
      * @return true if sent successfully, false otherwise
      */
     public static boolean sendEmail(String toEmail, String subject, String htmlBody) {
@@ -27,6 +31,9 @@ public class EmailUtil {
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
         props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.connectiontimeout", "10000");
+        props.put("mail.smtp.timeout", "10000");
+        props.put("mail.smtp.writetimeout", "10000");
 
         Session session = Session.getInstance(props, new Authenticator() {
             @Override
